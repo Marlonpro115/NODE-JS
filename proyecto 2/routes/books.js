a4a4cb0df1043e98146be992e10dd293b3069253
Marlonpro115 <113154160+Marlonpro115@users.noreply.github.com>
2025-05-09 19:33:14 -0500

    Add files via upload

---
+++ proyecto 2/routes/books.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const router = express.Router();
+const dbConn = require('../lib/db');
+
+// display books page
+router.get('/', function (req, res, next) {
+    dbConn.query('SELECT * FROM books ORDER BY id desc', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            // render to views/index.ejs
+            res.render('index', { data: '' });
+        } else {
+            // render to views/index.ejs
+            res.render('index', { data: rows });
+        }
+    });
+});
+
+// display add book page
+router.get('/add', function (req, res, next) {
+    res.render('add', {
+        name: '',
+        author: ''
+    });
+});
+
+// add a new book
+router.post('/add', function (req, res, next) {
+    let name = req.body.name;
+    let author = req.body.author;
+    let errors = false;
+
+    if (name.length === 0 || author.length === 0) {
+        errors = true;
+        req.flash('error', "Please enter name and author");
+        res.render('add', {
+            name: name,
+            author: author
+        });
+    }
+
+    if (!errors) {
+        let form_data = { name, author };
+
+        dbConn.query('INSERT INTO books SET ?', form_data, function (err, result) {
+            if (err) {
+                req.flash('error', err);
+                res.render('add', {
+                    name: form_data.name,
+                    author: form_data.author
+                });
+            } else {
+                req.flash('success', 'Book successfully added');
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+// display edit book page
+router.get('/edit/:id', function (req, res, next) {
+    let id = req.params.id;
+
+    dbConn.query('SELECT * FROM books WHERE id = ?', [id], function (err, rows) {
+        if (err) throw err;
+
+        if (rows.length <= 0) {
+            req.flash('error', 'Book not found with id = ' + id);
+            res.redirect('/');
+        } else {
+            res.render('edit', {
+                title: 'Edit Book',
+                id: rows[0].id,
+                name: rows[0].name,
+                author: rows[0].author
+            });
+        }
+    });
+});
+
+// update book data
+router.post('/update/:id', function (req, res, next) {
+    let id = req.params.id;
+    let name = req.body.name;
+    let author = req.body.author;
+    let errors = false;
+
+    if (name.length === 0 || author.length === 0) {
+        errors = true;
+        req.flash('error', "Please enter name and author");
+        res.render('edit', {
+            id,
+            name,
+            author
+        });
+    }
+
+    if (!errors) {
+        let form_data = { name, author };
+
+        dbConn.query('UPDATE books SET ? WHERE id = ?', [form_data, id], function (err, result) {
+            if (err) {
+                req.flash('error', err);
+                res.render('edit', {
+                    id,
+                    name: form_data.name,
+                    author: form_data.author
+                });
+            } else {
+                req.flash('success', 'Book successfully updated');
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+// delete book
+router.get('/delete/:id', function (req, res, next) {
+    let id = req.params.id;
+
+    dbConn.query('DELETE FROM books WHERE id = ?', [id], function (err, result) {
+        if (err) {
+            req.flash('error', err);
+            res.redirect('/');
+        } else {
+            req.flash('success', 'Book successfully deleted! ID = ' + id);
+            res.redirect('/');
+        }
+    });
+});
+
+module.exports = router;
